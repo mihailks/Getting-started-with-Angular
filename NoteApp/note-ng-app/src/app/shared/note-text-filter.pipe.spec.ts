6184979a60f7d81e3c17ff933ac3887ce960446e
Mihail Slavov <mihail_slavov@icloud.com>
2024-01-23 11:56:36 +0200

    change feedback page

--- NoteApp/note-ng-app/src/app/shared/note-text-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NoteTextFilterPipe } from './note-text-filter.pipe';
-
-describe('NoteTextFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NoteTextFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
